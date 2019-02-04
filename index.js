32cf1879c36d54abc20224df53f3b53f6f795369
sudanpsg <sudanpsg@gmail.com>
2019-02-03 22:32:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const _ = require("lodash");
 const express = require("express");
 const app = express();
-
+//adding commment for pull request
 app.get("/", (req, res) => {
   res.send({ Name: "hari", age: 25, skill: "cloud" });
 });
